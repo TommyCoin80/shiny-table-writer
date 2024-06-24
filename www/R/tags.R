64adde635aedefd47474ccf067a348b7e38da181
Tom Coin <thomasjcoin@gmail.com>
2024-06-24 11:23:13 -0400

    Update tags.R

--- www/R/tags.R
+++ www/R/tags.R
@@ -5,6 +5,6 @@ tagList(
   tags$head(
     tags$script(src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"),
     tags$script(HTML("WebFont.load({google: {families: ['Play']}});")),
-    tags$link(rel = "stylesheet", type = "text/css", href = "AA_STANDARD/CSS/aaStyle.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "CSS/aaStyle.css")
   )
 )
